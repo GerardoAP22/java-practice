3d96624bc978e1033f02b931abdfc625a370e44f
GerardoAP22 <garriazapantaleon@colgate.edu>
2024-12-14 16:19:21 -0500

    simple excercises

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
-
     public static void main(String[] args) {
+        System.out.println("\"I love pizza\""); // sysout shortcut
+
+        int x = 123;
+        long y = 123456798032452L; // Need to end a long initialization with L
+        float z = 3.14f; // Need to add the f like long
+        String name = "Gerardo";
+
+        System.out.println("My number is " + x);
+        System.out.println(y + ", " + z);
+        System.out.println("My name is " + name);
 
     }
 }
